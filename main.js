caa515c33497633565d47d44d4790f809f6cbc96
shilichev <52910393+shilichev@users.noreply.github.com>
2021-03-22 23:28:47 +0300

    ready to refactoring

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 $("#header").prepend(
   '<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>'
 );
-
+$(".dropdown").prepend('<div class="arrow-down">▼</div>');
+$(".box").prepend('<div class="arrow-down-second">▼</div>');
 $("#menu-icon").on("click", function () {
   $("nav").slideToggle();
   $(this).toggleClass("active");
-  console.log(2)
 });
-$("#mega-dropdown").on("click", function () {
-  console.log(1);
+$(".arrow-down").on("click", function () {
+  let div = $(this).parent();
+  let ul = div.siblings();
+  for (i = 0; i < ul.find(".secondChild").length; i++) {
+    $(ul.find(".secondChild")[i]).toggleClass("expand");
+  }
+});
+$(".arrow-down-second").on("click", function () {
+  let div = $(this).parent();
+  let ul = div.siblings();
+  for (i = 0; i < ul.find(".thirdChild").length; i++) {
+    $(ul.find(".thirdChild")[i]).toggleClass("expand");
+  }
 });
