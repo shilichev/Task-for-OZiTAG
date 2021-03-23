cadae42ffea4cd754c774d072eb439e68fa900bd
shilichev <52910393+shilichev@users.noreply.github.com>
2021-03-23 18:10:46 +0300

    task has finished

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 $("#header").prepend(
   '<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>'
 );
-$(".dropdown").prepend('<div class="arrow-down">▼</div>');//создаем стрелочку в верхнем li
-$(".box").prepend('<div class="arrow-down-second">▼</div>');// создаем стрелочку в дочернем li
-$("#menu-icon").on("click", function () {//открытие меню по клику
+
+$(".dropdown").prepend('<div class="arrow-down">▼</div>');
+//create an arrow in the parent li
+
+$(".box").prepend('<div class="arrow-down-second">▼</div>');
+//create an arrow in the child li
+
+$("#menu-icon").on("click", function () {
+  //open menu on click
   $("nav").slideToggle();
   $(this).toggleClass("active");
 });
-$(".arrow-down").on("click", function () {// открытие подменю при нажатии стрелочки
-  let div = $(this).parent();// обращаемся к родителю
-  let ul = div.siblings();// обращаемся к сестре
-  for (i = 0; i < ul.find(".secondChild").length; i++) {//проходим по всем элементам которые нужно открыть 
-    $(ul.find(".secondChild")[i]).toggleClass("expand");
+
+$(".arrow-down").on("click", function () {
+  // opening a submenu by pressing the arrow
+  let div = $(this).parent(); // contact the parent
+  let ul = div.siblings(); // contact the syblings
+  for (i = 0; i < ul.find(".second-сhild").length; i++) {
+    //go through all the elements that need to be opened
+    $(ul.find(".second-сhild")[i]).toggleClass("expand");
   }
 });
+
 $(".arrow-down-second").on("click", function () {
+  //works like method before
   let div = $(this).parent();
   let ul = div.siblings();
-  for (i = 0; i < ul.find(".thirdChild").length; i++) {
-    $(ul.find(".thirdChild")[i]).toggleClass("expand");
+  for (i = 0; i < ul.find(".third-сhild").length; i++) {
+    $(ul.find(".third-сhild")[i]).toggleClass("expand");
   }
 });
